bcbd4404d3b46e051e9c48a613012f20f9618d32
Arthur Ming <guobin.ming@pwc.com>
2020-04-19 22:59:28 +0800

    build: use expressjs as web app

--- server.ts
+++ server.ts
@@ -1,1 +1,7 @@
-console.log('hello world');
+import express from 'express';
+
+const server = express();
+
+server.use('/ping', (res, req) => req.json('pong'));
+
+server.listen(8080, () => console.log(`The server has started on port 8080`));
