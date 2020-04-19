703e5cd087d773cef1f4aa92413a762853350c4f
Arthur Ming <guobin.ming@pwc.com>
2020-04-19 23:19:25 +0800

    ci: use commit-msg git hooks and commitlin to check commit message

---
+++ tools/commitlint-config.js
@@ -0,0 +1,14 @@
+"module.exports = {extends: ['@commitlint/config-conventional']}"
+
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+			'type-enum': [2, 'always', [
+					"feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
+			]],
+			'subject-full-stop': [0, 'never'],
+      'subject-case': [0, 'never'],
+      'header-max-length': [2, 'always', '120'],
+      'scope-case': [0, 'never']
+	}
+};
