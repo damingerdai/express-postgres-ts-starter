fbba05b7759a1f7e3c3990da3ea739c20a307370
Arthur Ming <guobin.ming@pwc.com>
2020-04-26 21:51:52 +0800

    refactor: rewrite the route implements way

--- server.ts
+++ server.ts
@@ -1,43 +1,11 @@
 import express from 'express';
-import JoiValidator from 'express-joi-validation';
-import ExpressRouter from 'express-promise-router';
 
-import { routes } from './src/routes';
+import { router } from './src/routes';
 import { config } from 'src/config';
 
 const server = express();
 
 const serverConfig = config.server;
-const router = ExpressRouter(serverConfig.router);
-
-routes.forEach(route => {
-	const schema = route?.schema;
-	const next = (req, res, nxt) => nxt();
-
-	const validateBody = schema?.body ? JoiValidator().body(schema.body) : next;
-	const validateParams = schema?.params ? JoiValidator().params(schema.params) : next;
-	const validateQuery = schema?.query ? JoiValidator().query(schema.query) : next;
-
-	switch (route.method) {
-		case 'GET':
-			router.get(route.path, validateBody, validateParams, validateQuery, route.controller);
-			break;
-		case 'POST':
-			router.post(route.path, validateBody, validateParams, validateQuery, route.controller);
-			break;
-		case 'PUT':
-			router.put(route.path, validateBody, validateParams, validateQuery, route.controller);
-			break;
-		case 'PATCH':
-			router.patch(route.path, validateBody, validateParams, validateQuery, route.controller);
-			break;
-		case 'DELETE':
-			router.delete(route.path, validateBody, validateParams, validateQuery, route.controller);
-			break;
-		default:
-			throw new Error(`Failed to load route. Invalid method: ${route.method} for path ${route.path}`);
-	}
-})
 
 server.use('/', router);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,43 @@
+import JoiValidator from 'express-joi-validation';
+import ExpressRouter from 'express-promise-router';
+
 import { Ping } from './ping';
+import { config } from 'src/config';
+
+const expressRouter = ExpressRouter(config?.server?.router);
 
 export const routes = [Ping];
+
+routes.forEach(route => {
+	const schema = route?.schema;
+	const next = (req, res, nxt) => nxt();
+
+	const validateBody = schema?.body ? JoiValidator().body(schema.body) : next;
+	const validateParams = schema?.params ? JoiValidator().params(schema.params) : next;
+	const validateQuery = schema?.query ? JoiValidator().query(schema.query) : next;
+
+	switch (route.method) {
+		case 'GET':
+			expressRouter.get(route.path, validateBody, validateParams, validateQuery, route.controller);
+			break;
+		case 'POST':
+			expressRouter.post(route.path, validateBody, validateParams, validateQuery, route.controller);
+			break;
+		case 'PUT':
+			expressRouter.put(route.path, validateBody, validateParams, validateQuery, route.controller);
+			break;
+		case 'PATCH':
+			expressRouter.patch(route.path, validateBody, validateParams, validateQuery, route.controller);
+			break;
+		case 'DELETE':
+			expressRouter.delete(route.path, validateBody, validateParams, validateQuery, route.controller);
+			break;
+		default:
+			throw new Error(`Failed to load route. Invalid method: ${route.method} for path ${route.path}`);
+	}
+})
+
+export const router = expressRouter;
+
+
+
