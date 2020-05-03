cfb958788a0bfc560e7400627ec4d4a19a1669f9
Arthur Ming <guobin.ming@pwc.com>
2020-05-03 17:03:50 +0800

    feat: add db config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,32 @@
+import { camelCase, snakeCase } from 'src/lib';
+
+const specialChars = ['*'];
+
+const convertToCase = (val, func) => {
+	if (specialChars.includes(val)) return val;
+
+	return func(val);
+};
+
 export const config = {
+	db: {
+		charset: 'utf8',
+		client: 'pg',
+		debug: process.env.DEBUG || false,
+		connection: {
+			host: process.env.POSTGRES_HOST || 'db',
+			port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+			user: process.env.POSTGRES_USER || 'postgres',
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.NODE_ENV === 'test' ? 'postgres_test' : process.env.POSTGRES_DB_NAME || 'postgres'
+		},
+		pool: {
+			min: parseInt(process.env.POSTGRES_POOL_MIN, 10) || 2,
+			max: parseInt(process.env.POSTGRES_POOL_MAX, 10) || 10
+		},
+		postProcessResponse: (result, queryContext) => convertToCase(result, camelCase),
+		wrapIdentifier: (value, origImpl, queryContext) => origImpl(convertToCase(value, snakeCase))
+	},
 	server: {
 		port: process.env.APP_PORT || 3000,
 		router: {
---
+++ src/lib/changeCase.ts
@@ -0,0 +1,21 @@
+import * as _ from 'lodash';
+
+const transform = (data, func) => {
+	if (!data) {
+		return data;
+	} else if (Array.isArray(data)) {
+		return data.map(elem => transform(elem, func));
+	} else if (typeof data === 'object') {
+		return _.mapKeys(data, (value, key) => transform(key, func));
+	} else {
+		return func(data);
+	}
+};
+
+export const camelCase = data => {
+	return transform(data, _.camelCase);
+};
+
+export const snakeCase = data => {
+	return transform(data, _.snakeCase);
+};
