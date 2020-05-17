0bb5265ee52b48be1d8cf5e798c0bd7dd08096d9
damingeridai <mingguobin@live.com>
2020-05-17 21:00:54 +0800

    feat: add helmet middleware support

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,13 @@
 import cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
+
 import { BodyParserMiddleware } from './body-parser';
 
 export const middlewares = [
-    BodyParserMiddleware, cookieParser()
+    BodyParserMiddleware, 
+    cookieParser(),
+    helmet.noShiff(),
+    helmet.hsts({
+        maxAge: 518400
+    })
 ];
