8972122b92e92554bb0d2b91f5b8ac188e6d8de8
Arthur Ming <guobin.ming@pwc.com>
2020-07-06 23:44:21 +0800

    test: add unit test for change case functions

--- src/lib/__tests__/change-case.spec.ts
+++ src/lib/__tests__/change-case.spec.ts
@@ -1,5 +1,21 @@
-describe('redis-porter', () => {
-	it('hello world', () => {
-		console.log('hello world');
+import { camelCase, snakeCase } from '../change-case';
+
+describe('change case', () => {
+	test('camelCase', () => {
+		expect(camelCase(null)).toBeNull();
+		expect(camelCase(undefined)).toBeUndefined();
+		expect(camelCase(['A', 'b'])).toStrictEqual(['a', 'b']);
+		expect(camelCase({ A: 'a', b: 'B' })).toEqual({ a: 'a', b: 'B' });
+		expect(camelCase('AbC')).toBe('abC');
+		expect(camelCase('Foo Bar')).toBe('fooBar');
+		expect(camelCase('--foo-Bar--')).toBe('fooBar');
+		expect(camelCase('__FOO_BAR__')).toBe('fooBar');
+	});
+	test('snakeCase', () => {
+		expect(camelCase(null)).toBeNull();
+		expect(camelCase(undefined)).toBeUndefined();
+		expect(snakeCase('Foo Bar')).toBe('foo_bar');
+		expect(snakeCase('--foo-Bar--')).toBe('foo_bar');
+		expect(snakeCase('__FOO_BAR__')).toBe('foo_bar');
 	});
 });
