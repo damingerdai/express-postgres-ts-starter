3f6d3b4d5443bfe8b368b35df30598258566c420
Arthur Ming <mingguobin@live.com>
2022-10-24 11:20:53 +0800

    feat: add eslint fix in lint-staged and run more check in pre-push git hook (#215)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -18,5 +18,8 @@ module.exports = {
 		const prettier = `prettier --write -- ${filenames.join(' ')}`;
 		const git = `git add ${filenames.join(' ')}`;
 		return [prettier, git];
-	}
+	},
+	'**/*.(j|t)s?(x)': filenames => filenames.length > 10
+		? 'yarn lint:ts --fix'
+		: `eslint --format stylish ${filenames.join(' ')} --fix`
 };
